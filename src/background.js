c8f1fd84ded6559426a1d53afbc87c62620b69af
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-01-09 16:47:27 +0900

    reformat by prettier

--- src/background.js
+++ src/background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'saveContent') {
-        console.log('Received content:', message);
-        const storageKey = `perplexity_${message.session}`;
-        
-        // Required for async response
-        return true;
-    }
+  if (message.action === "saveContent") {
+    console.log("Received content:", message);
+    const storageKey = `perplexity_${message.session}`;
+
+    // Required for async response
+    return true;
+  }
 });
