15372ed39beca9374c2d5251136a384e02b8d8c3
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-01-09 22:29:49 +0900

    Delete deprecated read feature and working cited answer

--- src/background.js
+++ src/background.js
@@ -1,13 +1,3 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "saveContent") {
-    console.log("Received content:", message);
-    const storageKey = `perplexity_${message.session}`;
-
-    // Required for async response
-    return true;
-  }
-});
-
 chrome.webRequest.onCompleted.addListener(
   (details) => {
       console.log("Perplexity Conversation Answer Done");
@@ -34,3 +24,43 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
   }
 });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'webPageLoaded') {
+    const currentUrl = message.data;
+    console.log('Web page loaded:', currentUrl);
+    
+    chrome.storage.local.get('perplexity_cluehunter_recent', (result) => {
+      const recent = result['perplexity_cluehunter_recent'];
+      if (recent && compareURLs(recent.url,currentUrl)) {
+        console.log('Found recent:', recent);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            action: 'highlightCitation',
+            data: recent.answer
+          });
+        });
+      }
+    });
+  }
+});
+
+function compareURLs(url1, url2) {
+  const parseURL = (url) => {
+    try {
+      return new URL(url);
+    } catch {
+      // If parsing fails, try adding a protocol
+      return new URL(`http://${url}`);
+    }
+  };
+
+  const normalizeURL = (url) => {
+    const parsedURL = parseURL(url);
+    let hostname = parsedURL.hostname.replace(/^www\./, '');
+    let pathname = parsedURL.pathname.replace(/\/$/, '');
+    return `${hostname}${pathname}`;
+  };
+
+  return normalizeURL(url1) === normalizeURL(url2);
+}
--- src/content.js
+++ src/content.js
@@ -1,6 +1,17 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "highlightCitation") {
+    console.log("Highlighting citation:", message.data);
+    // TODO: Highlight the citation
+  }
+});
+
 if (isPerplexity(window.location.href)) {
   document.addEventListener("click", handleClick, true);
 } else {
+  chrome.runtime.sendMessage({
+    action: "webPageLoaded",
+    data: window.location.href,
+  });
   console.log(document.body.innerText); // Parsing
 }
 
@@ -123,97 +134,6 @@ function getNthSibling(element, n) {
   }
 
   return current;
-}
-
-// TODO: Implement the listener when there is a corresponding 
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "runPerplexityMain") {
-    // perplexity_main();
-  }
-});
-
-function perplexity_main() {
-  //   // Set up mutation observer to track dynamic content
-  //   const observer = new MutationObserver((mutations) => {
-  //     mutations.forEach((mutation) => {
-  //       if (mutation.addedNodes.length) {
-  //         extractPerplexityContent();
-  //       }
-  //     });
-  //   });
-
-  //   // Start observing the main content area
-  //   observer.observe(document.body, {
-  //     childList: true,
-  //     subtree: true,
-  //   });
-
-  document.addEventListener("click", handleClick, true);
-
-  // Initial content extraction
-  extractPerplexityContent();
-}
-
-function extractPerplexityContent() {
-  try {
-    const conversationNodes = getConversationNodes(document);
-    const dataList = conversationNodes.map(extractData);
-    console.log("Perplexity data:", dataList);
-
-    // Optional: Send to background script
-    chrome.runtime.sendMessage({
-      action: "saveContent",
-      session: getSessionName(window.location.href),
-      content: dataList,
-    });
-  } catch (error) {
-    console.error("Error extracting content:", error);
-  }
-}
-
-function extractData(node) {
-  return {
-    answer_citations: extractAnswerCitations(node),
-  };
-}
-
-function getConversationNodes(doc) {
-  // Pass DOM document as input.
-  // Returns list of conversation nodes.
-  const nodeList = doc.querySelectorAll(
-    ".pb-lg.border-borderMain\\/50.ring-borderMain\\/50.divide-borderMain\\/50.dark\\:divide-borderMainDark\\/50.dark\\:ring-borderMainDark\\/50.dark\\:border-borderMainDark\\/50.bg-transparent"
-  );
-  return Array.from(nodeList);
-}
-
-function extractAnswerCitations(node) {
-  const selector =
-    ".prose.dark\\:prose-invert.inline.leading-normal.break-words.min-w-0.\\[word-break\\:break-word\\]";
-  const answerCitationNode = node.querySelector(selector); // Contains all answer and citations under this question
-  const spanNodes = Array.from(answerCitationNode.querySelectorAll("span"));
-  const extractedSpans = spanNodes
-    .map(extractSpan)
-    .filter((span) => span.value !== null);
-  // Now need to make an answer-citation pairs
-  let currentIdx = -1;
-  let answerCitationPairs = [];
-  for (let i = 0; i < extractedSpans.length; i++) {
-    if (
-      extractedSpans[i].type === spanTypes.SENTENCE &&
-      extractedSpans[i + 1].type === spanTypes.CITATION
-    ) {
-      currentIdx++;
-      answerCitationPairs[currentIdx] = {
-        answer: extractedSpans[i].value,
-        citation: [],
-      };
-    }
-    if (extractedSpans[i].type === spanTypes.CITATION) {
-      answerCitationPairs[currentIdx].citation.push(extractedSpans[i].value);
-    }
-  }
-  return answerCitationPairs;
 }
 
 const spanTypes = Object.freeze({
