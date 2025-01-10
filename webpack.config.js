041cbf5a5243edbbde474a0ba79ad6f23e565d06
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-01-10 19:08:03 +0900

    Found working solution with refactor webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,56 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: {
-        background: './src/background.js',
-        popup: './src/popup.js',
-        content: './src/content.js',
-        options: './src/options.js',
+  mode: "development",
+  devtool: "inline-source-map",
+  target: "webworker",
+  entry: {
+    background: "./src/background.js",
+    popup: "./src/popup.js",
+    content: "./src/content.js",
+    options: "./src/options.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    globalObject: "this",
+  },
+  resolve: {
+    alias: {
+      "@huggingface/transformers": path.resolve(
+        __dirname,
+        "node_modules/@huggingface/transformers"
+      ),
     },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js',
-    },
-    resolve: {
-        alias: {
-            '@huggingface/transformers': path.resolve(__dirname, 'node_modules/@huggingface/transformers')
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/popup.html',
-            filename: 'popup.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/options.html',
-            filename: 'options.html',
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: "public",
-                    to: "." // Copies to build folder
-                },
-                {
-                    from: "src/popup.css",
-                    to: "popup.css"
-                }
-            ],
-        })
-    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/popup.html",
+      filename: "popup.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/options.html",
+      filename: "options.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: ".", // Copies to build folder
+        },
+        {
+          from: "src/popup.css",
+          to: "popup.css",
+        },
+      ],
+    }),
+  ],
 };
 
 export default config;
